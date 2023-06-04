c7d78e959ee5afe82aa2b84ec1f8f9d02a71249d
mac-deep <deepmac.25@gmail.com>
2023-06-05 00:42:01 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen flex flex-col justify-center items-center">
+      <h1 className="text-4xl font-bold text-center">
+        React + Typescript + SWC + Tailwindcss + Vite.js
+      </h1>
+    </div>
+  );
+}
+
+export default App;
